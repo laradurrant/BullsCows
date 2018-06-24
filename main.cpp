de7f282ef977333b3445794f885293a0465bf824
Lara <lara.durrant@gmail.com>
2018-06-24 11:42:55 -0700

    Functions Added
    Tidied up the code by adding two new functions

--- main.cpp
+++ main.cpp
@@ -4,26 +4,41 @@
 
 using namespace std;
 
+void PrintIntro();
+void GetGuess();
+
 int main() 
 {
+	PrintIntro();
+    
+	// This will get a guess from the player
+	GetGuess();
+
+	return 0;
+}
+
+
+void PrintIntro() {
 	// Sets the maximum word length
 	constexpr int WORD_LENGTH = 5;
-	
+
 	// Introduce the player to the game
 	cout << "..........................\n";
 	cout << "Welcome to Bulls and Cows!\n";
 	cout << "..........................\n";
 	cout << "This is a fun word game / puzzle game!\n";
 	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
-    
-	// This will get a guess from the player
+	return;
+}
+
+void GetGuess() {
+
 	string Guess = "";
-	
+
 	cout << "Enter your guess here: \n";
-	cin >> Guess;
-	cout << "I think you wrote: " << Guess << ".\n";
+	getline(cin, Guess);
+	cout << "I think you wrote: " << Guess << "\n";
 	cout << "Is that correct?\n";
 	cout << endl;
-	
-	return 0;
+	return;
 }
